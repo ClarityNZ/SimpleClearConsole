993f3828a299e65cec3f0c4cf2b6fe19eb9c535f
ClarityNZ <50315458+ClarityNZ@users.noreply.github.com>
2020-12-09 23:18:44 +1300

    Source cs added

---
+++ SimpleClearConsole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine;
+
+namespace MyProject
+{
+    public class SimpleClearConsole:MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Log("SimpleClearConsole is active, press F4 to clear console!");
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F4)){
+                Console.Clear();
+            }
+        }
+    }
+}
